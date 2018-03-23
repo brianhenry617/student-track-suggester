4fda96e3980a97ea0e668e16876e5c011d9801a0
Brian Henry <94brianhenry@gmail.com>
2018-03-23 11:34:45 -0700

    Fix a few things and add a few pages, it will help with giving detail by having info on a separate page

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,16 @@
 $(document).ready(function(){
-  // alert("hi");
 
-  $("#fields").change(function() {
-    var month = parseInt($("select#fields").val());
-    if (month === 0) {
-      $('#january').hide();
-      $('#february').hide();
-      $('#march').hide();
-      $('#april').hide();
-      $('#may').hide();
-      $('#june').hide();
-      $('#july').hide();
-      $('#august').hide();
-      $('#september').hide();
-      $('#october').hide();
-      $('#november').hide();
-      $('#december').hide();
+  $("#typeOfDev").change(function() {
+    var dev = parseInt($("select#typeOfDev").val());
+    if (dev === 1) {
+      $('#option1').hide();
+      $('#frontEnd').show();
+      $('#backEnd').hide();
     }
-    if (month === 1) {
-      $('#january').show();
-      $('#february').hide();
-      $('#march').hide();
-      $('#april').hide();
-      $('#may').hide();
-      $('#june').hide();
-      $('#july').hide();
-      $('#august').hide();
-      $('#september').hide();
-      $('#october').hide();
-      $('#november').hide();
-      $('#december').hide();
-    });
-
-
-// ready
-//
-// prompt Hello name
-//
-// prefFb
-// bussinessOnly===dataBase
-//
-//
-// new page
-// -    === -
-// - === -
-// - === -
-// - === -
-// - === -
+  else if (dev === 2) {
+    $('#option1').hide();
+    $('#frontEnd').hide();
+    $('#backEnd').show();
+  }
+ });
+});
